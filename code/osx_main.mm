ede1cefa7bf61cf4dd013b063e9f19babf4c378b
ronbarrantes <ronbarrantes@gmail.com>
2023-07-12 01:48:39 -0700

    Got a window

--- code/osx_main.mm
+++ code/osx_main.mm
@@ -5,24 +5,53 @@
 #import <stdio.h>
 #include <AppKit/AppKit.h>
 
-int main (int argc, const char * argv[]){
-    
-    // ALLOCATE a window and show it
-    while(true){
-      NSEvent* event;
-      do {
-        event = [NSApp nextEventMatchingMask: NSEventMaskAny
-          untilDate: nil
-          inMode: NSDefaultRunLoopMode
-          dequeue: YES];
-        switch([event type]){
-          default:
-            [NSApp sendEvent: event];
-        }
-      }
+static float GlobalRenderingWidth = 1024;
+static float GlobalRenderingHeight = 768;
 
-      while(event != nil);
+
+int main (int argc, const char * argv[]){
+
+  NSRect screenRect = [[NSScreen mainScreen] frame];
+
+  NSRect windowRect = NSMakeRect(
+      (screenRect.size.width - GlobalRenderingWidth) / 2,
+      (screenRect.size.height - GlobalRenderingHeight) / 2,
+      GlobalRenderingWidth,
+      GlobalRenderingHeight
+    );
+
+  NSWindow *window = [
+    [NSWindow alloc]
+      initWithContentRect: windowRect
+      styleMask:  NSWindowStyleMaskTitled |
+                  NSWindowStyleMaskClosable |
+                  NSWindowStyleMaskMiniaturizable |
+                  NSWindowStyleMaskResizable
+                  // NSWindowStyleMaskBorderless
+      backing: NSBackingStoreBuffered
+      defer: NO
+    ];
+
+  [window setBackgroundColor: NSColor.redColor];
+  [window setTitle: @"Handmade Hero"];
+  [window makeKeyAndOrderFront: nil];
+
+  // ALLOCATE a window and show it
+  while(true){
+    NSEvent* event;
+    do {
+      event = [NSApp nextEventMatchingMask: NSEventMaskAny
+        untilDate: nil
+        inMode: NSDefaultRunLoopMode
+        dequeue: YES];
+      switch([event type]){
+        default:
+          [NSApp sendEvent: event];
+      }
     }
 
-    printf("Handmade Hero app");
+    while(event != nil);
+  }
+
+  printf("Handmade finished running");
 }
